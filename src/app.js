b1cec1cd68cd38fe4d8c4ee2d656c0916dcdc575
rob <robmathew21@gmail.com>
2024-09-13 13:39:09 -0500

    added vercel.json, added jobs.js and required it in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const JOBS = require('./jobs');
 
 const app = express();
 
---
+++ src/jobs.js
@@ -0,0 +1,35 @@
+const JOBS = [ 
+    {
+        id: 1,
+        title: 'Frontend Developer',
+        location:'Bengaluru, India',
+        salary: '₹12,00,000',
+        posted: 'September 3, 2024'
+    },
+
+    {
+        id: 2,
+        title: 'Full Stack Developer',
+        location:'New Delhi, India',
+        salary: '₹15,00,000',
+        posted: 'September 6, 2024'
+    },
+
+    {
+        id: 3,
+        title: 'Data Scientist',
+        location:'San Francisco, USA',
+        salary: '$175,000',
+        posted: 'September 9, 2024'
+    },
+
+    {
+        id: 4,
+        title: 'Machine Learning Engineer',
+        location:'Remote',
+        salary: '$100,000',
+        posted: 'September 9, 2024'
+    }
+]
+
+module.exports = JOBS
