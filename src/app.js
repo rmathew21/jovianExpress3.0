b370dda4bb2d05f2abdd21eea36eb2ff4a5740bf
rob <robmathew21@gmail.com>
2024-09-13 12:55:57 -0500

    added pages/index, used path.join to send html, added nodemon

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname, 'pages/index.html'));
 });
 
 const port = process.env.PORT || 3000;
