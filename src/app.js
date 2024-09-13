976a5b418bd7d19210be4d0ed2c97a69c5ccf147
rob <robmathew21@gmail.com>
2024-09-13 15:33:31 -0500

    applied {{}} data from JOBS to index, created job.mustache

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path = require('path');
 const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+    res.render('index', { jobs: JOBS });
 });
 
 const port = process.env.PORT || 3000;
