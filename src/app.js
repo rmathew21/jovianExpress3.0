9b28fd0e9677fa12619c37508f524e2471250ad2
rob <robmathew21@gmail.com>
2024-09-14 02:13:48 -0500

    installed nodemailer, body-parser, and dotenv, added transporter info with gmail, created route for jobs/id/apply

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const JOBS = require('./jobs');
 const mustacheExpress = require('mustache-express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,11 +22,54 @@ app.get('/', (req, res) => {
 
 app.get('/jobs/:id', (req, res) => {
     const id = req.params.id;
-    console.log('req.params', req.params);
+    // console.log('req.params', req.params);
     const matchedJob = JOBS.find(job => job.id.toString() === id);
     console.log('matchedJob', matchedJob);
     res.render('job', {job:matchedJob});
 });
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: 
+    {
+        user: process.env.EMAIL_ID,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
+
+app.post('/jobs/:id/apply', (req, res) => {
+    const { name, email, phone, dob, coverletter } = req.body;
+    const id = req.params.id;
+    console.log('req.body', req.body);
+    const matchedJob = JOBS.find(job => job.id.toString() === id);
+    console.log('matchedJob', matchedJob);
+
+    const mailOptions = {
+        from: process.env.EMAIL_ID,
+        to: process.env.EMAIL_ID,
+        subject: `New Application for ${matchedJob.title}`,
+        html: `
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone:</strong> ${phone}</p>
+        <p><strong>Date of birth:</strong> ${dob}</p>
+        <p><strong>Cover Letter:</strong> ${coverletter}</p>
+        ` 
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).send('Email sent successfully');
+        }
+    });
+    
+})
 
 const port = process.env.PORT || 3000;
 
